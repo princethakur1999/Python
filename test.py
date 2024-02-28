18dc83f3bf2267112710f8628a54a46ba3eed555
shree kapote <shreekapote72@gmail.com>
2024-02-28 16:10:19 +0530

    test

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 print("A")
-<<<<<<< HEAD
 print('shreekapote')
 print('rajendra')
-=======
-shree
 print('shreekapote')
-
 print("Krishna was here!!")
-main
->>>>>>> 4473a74fe7b20207420611d093456c49aff3de44
