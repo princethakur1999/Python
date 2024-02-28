78fbd1b8384ed685f9cf92f05cc3900416c34cb7
Krack17 <krishnakokane17@gmail.com>
2024-02-28 15:54:07 +0530

    New add

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 print("A")
+<<<<<<< HEAD
 print("Krishna was here!!")
 print("Krishna was here again!")
+=======
+shree
+print('shreekapote')
+
+print("Krishna was here!!")
+main
+>>>>>>> a4c93f1e971af75070de08f7260c69598677de97
